24ba233b3d5375e3f8403ba6f93115a86997da63
snehildwivedi03 <snehildwivedi03@gmail.com>
2023-12-31 13:50:16 +0530

    Added the dark mode

--- 19-DarkMode/app.js
+++ 19-DarkMode/app.js
@@ -1,1 +1,7 @@
-console.log('dark mode');
+//toggleBtn is for dark mode switching
+
+const toggleBtn = document.querySelector(".btn");
+
+toggleBtn.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark-theme");
+});
--- 19-DarkMode/data.js
+++ 19-DarkMode/data.js
@@ -1,30 +1,38 @@
 const articles = [
   {
     id: 1,
-    title: 'the WET Codbase',
-    date: new Date(2020, 9, 4),
+    title: "Snehil Dwivedi",
+    date: new Date(2024, 0, 1), // January is 0-based in JavaScript dates
     length: 11,
-    snippet: `I'm baby ramps kombucha gluten-free ennui swag tattooed street art. Marfa biodiesel letterpress blue bottle subway tile, pop-up pok pok of.`,
+    snippet: `I am Snehil Dwivedi, a frontend developer and an enthusiastic learner. I enjoy building web applications and staying up-to-date with the latest technologies.`,
   },
   {
     id: 2,
-    title: 'goodby, clean code',
-    date: new Date(2019, 10, 22),
-    length: 5,
-    snippet: `Biodiesel artisan seitan plaid sriracha copper mug venmo shabby chic. Kickstarter raclette kombucha, yr post-ironic jianbing try-hard flexitarian vaporware normcore.`,
+    title: "Skills: HTML, CSS, JavaScript ,C/C++, Problem Solving",
+    date: new Date(2023, 0, 1),
+    length: 10,
+    snippet: `I have expertise in HTML, CSS, JavaScript, C/C++, and Problem Solving. Constantly improving my skills to deliver high-quality software solutions.`,
   },
   {
     id: 3,
-    title: 'my decade in review',
-    date: new Date(2018, 7, 11),
+    title: "Achievements: JS Projects, Websites, Internship",
+    date: new Date(2022, 6, 1), // Assuming this is the current date for achievements
     length: 5,
-    snippet: `Direct trade shabby chic four dollar toast, tilde actually try-hard bicycle rights aesthetic forage. Meditation keytar asymmetrical tacos artisan truffaut. Pabst jean shorts roof party scenester.`,
+    snippet: `I have successfully completed various JavaScript projects, built several websites, and gained practical experience through a content editor internship,and also excelled in Maths Olympiad.`,
   },
   {
     id: 4,
-    title: 'what are the react team principles',
-    date: new Date(2015, 5, 4),
-    length: 5,
-    snippet: `Selvage street art hammock affogato VHS. Mustache shaman literally wayfarers schlitz. Direct trade four loko narwhal VHS pop-up, chartreuse trust fund typewriter street art swag thundercats art party.`,
+    title: "Hobbies: Reading, Music, Exploring Startups",
+    date: new Date(2022, 0, 1),
+    length: 8,
+    snippet: `Apart from coding, I have a passion for reading, enjoy exploring different genres of music, and love keeping up with the latest trends in the startup world.`,
+  },
+  {
+    id: 5,
+    title:
+      "Education: Saint James Senior Secondary School and Birla Institute of Applied Sciences",
+    date: new Date(2022, 0, 1),
+    length: 10,
+    snippet: `I completed my schooling at Saint James Senior Secondary School and pursuing my college education in  Birla Institute of Applied Sciences (Bhimtal, Nainital (263136)).`,
   },
 ];
