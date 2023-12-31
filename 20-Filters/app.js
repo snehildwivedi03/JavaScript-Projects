8117f75002564f48d2c5ae831e04588a65f550c4
snehildwivedi03 <snehildwivedi03@gmail.com>
2023-12-31 16:23:30 +0530

    Added the basic functionality

--- 20-Filters/app.js
+++ 20-Filters/app.js
@@ -1,1 +1,20 @@
-console.log('filters project');
+let filteredProduct = [...products];
+const productsContainer = document.querySelector(".products-container");
+const displayProducts = () => {
+  //if statement
+
+  productsContainer.innerHTML = filteredProduct
+    .map(({ id, title, image, price }) => {
+      return ` <article class="product" data-id ="${id}">
+   <img
+     src="${image}"
+   />
+   <footer>
+     <h5 class="product-name">${title}a</h5>
+     <span class="product-price">₹${price}</span>
+   </footer>
+ </article>`;
+    })
+    .join("");
+};
+displayProducts();
