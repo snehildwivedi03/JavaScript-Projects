0814422872dee7b00818d90ec6784a1455d0415e
snehildwivedi03 <snehildwivedi03@gmail.com>
2023-12-31 14:26:21 +0530

    Complete Dark-Mode

--- 19-DarkMode/data.js
+++ 19-DarkMode/data.js
@@ -3,36 +3,35 @@ const articles = [
     id: 1,
     title: "Snehil Dwivedi",
     date: new Date(2024, 0, 1), // January is 0-based in JavaScript dates
-    length: 11,
+    length: 5,
     snippet: `I am Snehil Dwivedi, a frontend developer and an enthusiastic learner. I enjoy building web applications and staying up-to-date with the latest technologies.`,
   },
   {
     id: 2,
-    title: "Skills: HTML, CSS, JavaScript ,C/C++, Problem Solving",
+    title: "Skills",
     date: new Date(2023, 0, 1),
     length: 10,
     snippet: `I have expertise in HTML, CSS, JavaScript, C/C++, and Problem Solving. Constantly improving my skills to deliver high-quality software solutions.`,
   },
   {
     id: 3,
-    title: "Achievements: JS Projects, Websites, Internship",
+    title: "Achievements",
     date: new Date(2022, 6, 1), // Assuming this is the current date for achievements
-    length: 5,
+    length: 10,
     snippet: `I have successfully completed various JavaScript projects, built several websites, and gained practical experience through a content editor internship,and also excelled in Maths Olympiad.`,
   },
   {
     id: 4,
-    title: "Hobbies: Reading, Music, Exploring Startups",
+    title: "Hobbies",
     date: new Date(2022, 0, 1),
-    length: 8,
+    length: 2,
     snippet: `Apart from coding, I have a passion for reading, enjoy exploring different genres of music, and love keeping up with the latest trends in the startup world.`,
   },
   {
     id: 5,
-    title:
-      "Education: Saint James Senior Secondary School and Birla Institute of Applied Sciences",
+    title: "Education",
     date: new Date(2022, 0, 1),
-    length: 10,
+    length: 2,
     snippet: `I completed my schooling at Saint James Senior Secondary School and pursuing my college education in  Birla Institute of Applied Sciences (Bhimtal, Nainital (263136)).`,
   },
 ];
