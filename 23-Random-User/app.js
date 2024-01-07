29c775d8742903532d14148b6717cc7f7b65ef91
snehildwivedi03 <snehildwivedi03@gmail.com>
2024-01-07 18:32:31 +0530

    User

--- 23-Random-User/app.js
+++ 23-Random-User/app.js
@@ -1,10 +1,17 @@
 import get from "./utils/getElement.js";
-
-const URL = "https://randomuser.me/api/";
+import getUser from "./utils/fetchUser.js";
 
 const img = get(".user-img");
 const title = get(".user-title");
 const value = get(".user-value");
 const btn = get(".btn");
 const btns = [...document.querySelectorAll(".icon")];
-console.log(btns);
+
+const showUser = async () => {
+  //get user form api
+  const person = await getUser();
+  console.log(person);
+  //display api
+};
+window.addEventListener("DOMContentLoaded", showUser);
+btn.addEventListener("click", showUser);
