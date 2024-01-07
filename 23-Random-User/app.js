da250c75eec8c517e147498243403635a531a0fb
snehildwivedi03 <snehildwivedi03@gmail.com>
2024-01-07 21:13:03 +0530

    Random User final

--- 23-Random-User/app.js
+++ 23-Random-User/app.js
@@ -1,35 +1,16 @@
 import get from "./utils/getElement.js";
 import getUser from "./utils/fetchUser.js";
+import displayUser from "./utils/displayUser.js";
 
-const img = get(".user-img");
-const title = get(".user-title");
-const value = get(".user-value");
 const btn = get(".btn");
-const btns = [...document.querySelectorAll(".icon")];
-
-const displayUser = (person) => {
-  img.src = person.image;
-  value.textContent = person.name;
-  title.textContent = `My name is`;
-  btns.forEach((btn) => btn.classList.remove("active"));
-
-  btns[0].classList.add("active");
-  btns.forEach((btn) => {
-    const label = btn.dataset.label;
-    btn.addEventListener("click", () => {
-      title.textContent = `My ${label} is`;
-      value.textContent = person[label];
-      btns.forEach((btn) => btn.classList.remove("active"));
-      btn.classList.add("active");
-    });
-  });
-};
 
 const showUser = async () => {
-  //get user form api
+  // get user from api
   const person = await getUser();
   displayUser(person);
-  //display api
+
+  // display user
 };
+
 window.addEventListener("DOMContentLoaded", showUser);
 btn.addEventListener("click", showUser);
