c90bd0ca4ce0a1e48d83aa5880194138bb9389f0
snehildwivedi03 <snehildwivedi03@gmail.com>
2024-01-07 18:47:34 +0530

    User

--- 23-Random-User/app.js
+++ 23-Random-User/app.js
@@ -7,10 +7,28 @@ const value = get(".user-value");
 const btn = get(".btn");
 const btns = [...document.querySelectorAll(".icon")];
 
+const displayUser = (person) => {
+  img.src = person.image;
+  value.textContent = person.name;
+  title.textContent = `My name is`;
+  btns.forEach((btn) => btn.classList.remove("active"));
+
+  btns[0].classList.add("active");
+  btns.forEach((btn) => {
+    const label = btn.dataset.label;
+    btn.addEventListener("click", () => {
+      title.textContent = `My ${label} is`;
+      value.textContent = person[label];
+      btns.forEach((btn) => btn.classList.remove("active"));
+      btn.classList.add("active");
+    });
+  });
+};
+
 const showUser = async () => {
   //get user form api
   const person = await getUser();
-  console.log(person);
+  displayUser(person);
   //display api
 };
 window.addEventListener("DOMContentLoaded", showUser);
--- 23-Random-User/utils/fetchUser.js
+++ 23-Random-User/utils/fetchUser.js
@@ -1,14 +1,17 @@
 const URL = "https://randomuser.me/api/";
+
 const getUser = async () => {
   const response = await fetch(URL);
   const data = await response.json();
-  //destructure
+  // destructure
   const person = data.results[0];
   const { phone, email } = person;
   const { large: image } = person.picture;
   const { password } = person.login;
   const { first, last } = person.name;
-  const { dob: age } = person;
+  const {
+    dob: { age },
+  } = person;
   const {
     street: { number, name },
   } = person.location;
