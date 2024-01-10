6e645feb922566679f632ecbed03a378561e7e43
snehildwivedi03 <snehildwivedi03@gmail.com>
2024-01-10 20:26:35 +0530

    Pagiantion

--- 27-Pagination/app.js
+++ 27-Pagination/app.js
@@ -4,11 +4,44 @@ import paginate from "./paginate.js";
 import displayButtons from "./displayButtons.js";
 
 const title = document.querySelector(".section-title h1");
+const btnContainer = document.querySelector(".btn-container");
+
+let index = 0;
+let pages = [];
+
+const setupUI = () => {
+  displayFollowers(pages[index]);
+  displayButtons(btnContainer, pages, index);
+
+  // Smooth scroll to the top of the container
+  btnContainer.scrollIntoView({ behavior: "smooth" });
+};
 
 const init = async () => {
   const followers = await fetchFollowers();
-  displayFollowers(followers);
   title.textContent = "pagination";
+  pages = paginate(followers);
+  setupUI();
 };
+
+btnContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("btn-container")) return;
+  if (e.target.classList.contains("page-btn")) {
+    index = parseInt(e.target.dataset.index);
+  }
+  if (e.target.classList.contains("next-btn")) {
+    index++;
+    if (index > pages.length - 1) {
+      index = 0;
+    }
+  }
+  if (e.target.classList.contains("prev-btn")) {
+    index--;
+    if (index < 0) {
+      index = pages.length - 1;
+    }
+  }
+  setupUI();
+});
 
 window.addEventListener("load", init);
--- 27-Pagination/paginate.js
+++ 27-Pagination/paginate.js
@@ -1,3 +1,12 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  const itemsPerPage = 9;
+  const numberOfPages = Math.ceil(followers.length / itemsPerPage);
 
-export default paginate
+  const newFollowers = Array.from({ length: numberOfPages }, (_, index) => {
+    const start = index * itemsPerPage;
+    return followers.slice(start, start + itemsPerPage);
+  });
+  return newFollowers;
+};
+
+export default paginate;
