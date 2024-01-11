3e8c10e189d6eee2bbfdbc9adb205c7e06e7af9d
snehildwivedi03 <snehildwivedi03@gmail.com>
2024-01-11 22:11:56 +0530

    Wiki

--- 28-Wiki-API/app.js
+++ 28-Wiki-API/app.js
@@ -1,1 +1,22 @@
-console.log('wiki starter');
+const url =
+  "https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=searchValue";
+
+const formDOM = document.querySelector(".form");
+const inputDOM = document.querySelector(".form-input");
+const resultDOM = document.querySelector(".results");
+
+formDOM.addEventListener("submit", (e) => {
+  // Fixed typo here
+  e.preventDefault();
+  const value = inputDOM.value;
+  if (!value) {
+    resultDOM.innerHTML = `<div class="error">please enter valid search term</div>`;
+    return;
+  }
+
+  fetchPages(value);
+});
+
+const fetchPages = async (searchValue) => {
+  console.log(searchValue);
+};
