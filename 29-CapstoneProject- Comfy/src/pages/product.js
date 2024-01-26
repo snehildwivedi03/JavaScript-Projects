bb0b6498aab3be7278cfada571993f15739e859f
snehildwivedi03 <snehildwivedi03@gmail.com>
2024-01-26 08:20:58 +0530

    Product

--- 29-CapstoneProject- Comfy/src/pages/product.js
+++ 29-CapstoneProject- Comfy/src/pages/product.js
@@ -29,6 +29,28 @@ window.addEventListener("DOMContentLoaded", async function () {
     const response = await fetch(`${singleProductUrl}${urlID}`);
     if (response.status >= 200 && response.status <= 299) {
       const product = await response.json();
+      //grab data
+      const { id, fields } = product;
+      productID = id;
+
+      const { name, company, price, colors, description } = fields;
+      const image = fields.image[0].thumbnails.large.url;
+
+      //set values
+
+      document.title = `${name.toUpperCase()} | Comfy`;
+      pageTitleDOM.textContent = `Home / ${name}`;
+      imgDOM.src = image;
+      titleDOM.textContent = name;
+      companyDOM.textContent = company;
+      priceDOM.textContent = formatPrice(price);
+      descDOM.textContent = description;
+      colors.forEach((color) => {
+        const span = document.createElement("span");
+        span.classList.add("product-color");
+        span.style.backgroundColor = `${color}`;
+        colorsDOM.appendChild(span);
+      });
     } else {
       console.log(response.status, response.statusText);
       centerDOM.innerHTML = `
@@ -45,4 +67,7 @@ sorry, something went wrong
   }
 
   loading.style.display = "none";
+});
+cartBtn.addEventListener("click", function () {
+  addToCart(productID);
 });
