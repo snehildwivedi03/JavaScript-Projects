747d2a24e388b4428f0ae1e917f7c5fa86744a5c
snehildwivedi03 <snehildwivedi03@gmail.com>
2024-01-26 12:23:15 +0530

    Cart

--- 29-CapstoneProject- Comfy/src/cart/setupCart.js
+++ 29-CapstoneProject- Comfy/src/cart/setupCart.js
@@ -18,24 +18,29 @@ let cart = getStorageItem("cart");
 
 export const addToCart = (id) => {
   let item = cart.find((cartItem) => cartItem.id === id);
+
   if (!item) {
     let product = findProduct(id);
-    //add item to the cart
+    // add item to the the
     product = { ...product, amount: 1 };
     cart = [...cart, product];
-    // add item to the DOM
+    // add item to the DOM;
     addToCartDOM(product);
   } else {
-    //update value
+    // update values
+    const amount = increaseAmount(id);
+    const items = [...cartItemsDOM.querySelectorAll(".cart-item-amount")];
+    const newAmount = items.find((value) => value.dataset.id === id);
+    newAmount.textContent = amount;
   }
   // add one to the item count
   displayCartItemCount();
-
-  //display cart totals
+  // display cart totals
   displayCartTotal();
-  //set caet in local storage
+  // set cart in local storage
+
   setStorageItem("cart", cart);
-  //more stuff
+  //more stuff coming up
   openCart();
 };
 function displayCartItemCount() {
@@ -48,21 +53,79 @@ function displayCartTotal() {
   let total = cart.reduce((total, cartItem) => {
     return (total += cartItem.price * cartItem.amount);
   }, 0);
-  cartTotalDOM.textContent = `Total : ${formatPrice(total)}`;
+  cartTotalDOM.textContent = `Total : ${formatPrice(total)} `;
 }
 function displayCartItemsDOM() {
-  cartItemsDOM.forEach((cartItem) => {
+  cart.forEach((cartItem) => {
     addToCartDOM(cartItem);
   });
 }
-function setupCartFunctionality() {}
+function removeItem(id) {
+  cart = cart.filter((cartItem) => cartItem.id !== id);
+}
+function increaseAmount(id) {
+  let newAmount;
+  cart = cart.map((cartItem) => {
+    if (cartItem.id === id) {
+      newAmount = cartItem.amount + 1;
+      cartItem = { ...cartItem, amount: newAmount };
+    }
+    return cartItem;
+  });
+  return newAmount;
+}
+function decreaseAmount(id) {
+  let newAmount;
+  cart = cart.map((cartItem) => {
+    if (cartItem.id === id) {
+      newAmount = cartItem.amount - 1;
+      cartItem = { ...cartItem, amount: newAmount };
+    }
+    return cartItem;
+  });
+  return newAmount;
+}
+
+function setupCartFunctionality() {
+  cartItemsDOM.addEventListener("click", function (e) {
+    const element = e.target;
+    const parent = e.target.parentElement;
+    const id = e.target.dataset.id;
+    const parentID = e.target.parentElement.dataset.id;
+    // remove
+    if (element.classList.contains("cart-item-remove-btn")) {
+      removeItem(id);
+      // parent.parentElement.remove();
+      element.parentElement.parentElement.remove();
+    }
+    // increase
+    if (parent.classList.contains("cart-item-increase-btn")) {
+      const newAmount = increaseAmount(parentID);
+      parent.nextElementSibling.textContent = newAmount;
+    }
+    // decrease
+    if (parent.classList.contains("cart-item-decrease-btn")) {
+      const newAmount = decreaseAmount(parentID);
+      if (newAmount === 0) {
+        removeItem(parentID);
+        parent.parentElement.parentElement.remove();
+      } else {
+        parent.previousElementSibling.textContent = newAmount;
+      }
+    }
+    displayCartItemCount();
+    displayCartTotal();
+    setStorageItem("cart", cart);
+  });
+}
 const init = () => {
   // display amount of cart items
   displayCartItemCount();
-  //display total
+  // display total
   displayCartTotal();
-  // add all cart items to the DOM
+  // add all cart items to the dom
   displayCartItemsDOM();
   // setup cart functionality
   setupCartFunctionality();
 };
+init();
